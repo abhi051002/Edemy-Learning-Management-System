9d29bfcd631e594c2f612d09e233c2c8f1e53331
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2025-06-07 11:16:24 +0530

    update Vite configuration for HMR client port and global definition

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['d298-103-182-10-179.ngrok-free.app']
+    hmr: {
+      clientPort: 443
+    }
   },
+  define: {
+    global: 'globalThis'
+  }
 })
